4f6fd4caa4c8394425dc29594b46f3a170a7a1e2
David Bean <heinzbeansss@gmail.com>
2023-11-08 18:16:14 +0000

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<h1 class="title">Music Library</h1>
+        <div class="nav">
+            <ul>
+                <a href="/project5-musiclibrary/pages/songs.php">
+                    <li class="nav-item">Songs</li>
+                </a>
+                <a href="/project5-musiclibrary/pages/artists.php">
+                    <li class="nav-item">Artists</li>
+                </a>
+                <a href="/project5-musiclibrary/pages/albums.php">
+                    <li class="nav-item">Albums</li>
+                </a>
+            </ul>
+        </div>
---
+++ pages/artists.php
@@ -0,0 +1,19 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../styleee.css" rel="stylesheet">
+    <title>Artists</title>
+</head>
+<body>
+    <?php
+    include('../includes/header.php');
+    ?>
+</body>
+</html>
